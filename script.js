e8082716853de625abbb48f23fe8768af6de6afe
MD MEHEDI HASAN RONY <ronyvaiyaauniqued01@gmail.com>
2023-05-31 23:07:44 +0600

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const btnHam = document.querySelector('.ham-btn');
+
+const btnTimes = document.querySelector('times-btn');
+
+const navBar = document.getElementById(nav-bar);
+
+btnHam.addEventListener('click', function(){
+    if(btnHam.className !== "")
+    {
+        btnHam.style.display = "none";
+        btnTimes.style.display = "block";
+        navBar.classList.add("show-nav");
+    }
+})
+
